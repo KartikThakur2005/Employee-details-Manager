7ccf15ce1e38321946fa123e97d6a78dcd9aa71c
Kartik2005 <kartikthakursaklani@gmail.com>
2024-09-10 21:16:31 +0530

    adding animation pop up

--- scirpt.js
+++ scirpt.js
@@ -1,11 +1,24 @@
+//  for our add pop up
 let box = document.querySelector("#box");
-let addbtn  = document.querySelector("#addbtn");
+let addbtn = document.querySelector("#addbtn");
 let add_user = document.querySelector("#add_user");
+let close_btn = document.querySelector("#close_but");
 
-let add_window = function()
-{
-   box.classList.add("hidden");
-   add_user.classList.remove("hidden");
-}     
+let add_window = function() {
+    box.classList.add("hidden");
+    box.classList.remove("visible");
+    add_user.classList.add("visible");
+    add_user.classList.remove("hidden");
+};
 
-addbtn.addEventListener("click",add_window)
+let clos_add = function() {
+    add_user.classList.add("hidden");
+    add_user.classList.remove("visible");
+    box.classList.add("visible");
+    box.classList.remove("hidden");
+};
+
+addbtn.addEventListener("click", add_window);
+close_btn.addEventListener("click", clos_add);
+
+// ---------------------------------------------
