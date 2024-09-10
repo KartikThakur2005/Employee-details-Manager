a8a6d89ad535b95e6cb74623c5ee582ba61da912
Kartik2005 <kartikthakursaklani@gmail.com>
2024-09-10 22:00:11 +0530

    adding local storage for user

--- scirpt.js
+++ scirpt.js
@@ -22,3 +22,30 @@ addbtn.addEventListener("click", add_window);
 close_btn.addEventListener("click", clos_add);
 
 // ---------------------------------------------
+
+//  Form registration
+let regForm = document.querySelector("#register-form");
+let allInput = regForm.querySelectorAll("INPUT"); // thsi return an list od all input 
+let allRegData = [];
+
+if(localStorage.getItem("allRegData")!= null)  // after reload our data will be saved here from local sorage
+{
+    allRegData = JSON.parse(localStorage.getItem("allRegData"));
+}
+
+regForm.onsubmit = (e) =>
+    {
+        e.preventDefault();  // this will not save data after reload
+       let
+        allRegData.push({
+            name:allInput[0].value,
+            email:allInput[1].value,
+            mobile:allInput[2].value,
+            dob:allInput[3].value,
+            password:allInput[4].value,
+        })
+      localStorage.setItem("allRegData", JSON.stringify(allRegData));
+
+    }
+
+
